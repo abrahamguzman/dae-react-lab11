9fbe7680f6488a1bbcfbd233282df2abb8ff68d5
Abraham Guzman <85902876+abrahamguzman@users.noreply.github.com>
2022-11-15 14:53:25 -0500

    1 commit -> mguzman

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      productos: [],
+      recuperado: true
+    }
+  }
+
+  componentWillMount() {
+    fetch('http://localhost:8000/api/producto/')
+      .then((response) => {
+        return response.json()
+      })
+      .then((prod) => {
+        this.setState({ productos: prod })
+      })    
+  }
+
+  mostrarTabla() {
+    return (
+      <div>
+        <table border="1">
+        <thead>
+          <tr>
+            <th>Código</th>
+            <th>Descripción</th>
+            <th>Precio</th>                    
+          </tr>
+        </thead>
+        <tbody>  
+          {this.state.productos.map(prod => {
+            return (
+              <tr key={ prod.codigo}>
+                <td>{ prod.codigo}</td>
+                <td>{ prod.descripcion}</td>
+                <td>{ prod.precio}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+        </table>
+      </div>
+    );
+  }
+  render() {
+    if (this.state.recuperado)
+      return this.mostrarTabla()
+    else
+      return (<div>recuperando datos...</div>)
+    ;
+  }
+  
 }
 
 export default App;
